0a6dcfd05e16a117aacf5e845e9c1542b03f2bde
marionm <redmat527@PC-MATHIS-MARION.localdomain>
2020-05-12 19:21:34 +0200

    Vérification des noms de fichier donnés en ligne de commande.

--- src/console.c
+++ src/console.c
@@ -4,60 +4,76 @@
 #include <string.h>
 #include <console.h>
 
-void print_wrong(char **argv)
-{
-	printf("Usage : \'%s [--options] <input_file>\'\n", argv[0]);
-	printf("Try : \'%s --help\' for more information.\n", argv[0]);
-}
-
-void print_help(char **argv)
-{
-	printf("Usage : \'%s [--options] <input_file>\'\n\n", argv[0]);
-	printf("Possible options are :\n");
-	printf("--outfile=output_file.jpg\n");
-	printf("Specifies the name for the ouptut file, which is by default set to <input_file>.jpg.\n\n");
-	printf("--sample=h1xv1,h2xv2,h3xv3\n");
-	printf("Specifies the sampling factors hxv for the 3 color componants (RGB), which are set by default to 1x1.\n");
-}
 
 struct jpeg *get_jpeg_from_console(int argc, char **argv)
 {
 	if (argc < 2) {
-		print_wrong(argv);
+		printf("Usage : \'%s [--options] <input_file>\'\n", argv[0]);
+		printf("Try : \'%s --help\' for more information.\n", argv[0]);
 		return NULL;
 	}
+	/* --help option */
 	if (strcmp(argv[1], "--help") == 0 && argc == 2) {
-		print_help(argv);
+		printf("Usage : \'%s [--options] <input_file>\'\n\n", argv[0]);
+		printf("Possible options are :\n");
+		printf("--outfile=output_file.jpg\n");
+		printf("Specifies the name for the ouptut file, which is by default set to <input_file>.jpg.\n\n");
+		printf("--sample=h1xv1,h2xv2,h3xv3\n");
+		printf("Specifies the sampling factors hxv for the 3 color componants (RGB), which are set by default to 1x1.\n");
 		return NULL;
 	}
+	/* flags to check that options are called only once */
 	bool outfile_flag = false;
 	bool sample_flag = false;
+	/* jpeg struct containing all the data extracted */
 	struct jpeg *infos = jpeg_create();
 	for (int i = 1; i < argc - 1; i++) {
-		if (strncmp(argv[i], "--outfile=", 10) == 0) {
+		/* --outfile option */
+		if (!strncmp(argv[i], "--outfile=", 10)) {
 			if (outfile_flag) {
-				printf("--outfile option should only be called once.\n");
+				printf("\'--outfile\' option should only be called once.\n");
 			}
-			if (argv[i][10] == '\0') {
-				printf("--outfile option cannot be empty.\n");
+			char *output_filename = &argv[i][10];
+			size_t name_length = strlen(output_filename);
+			if (name_length < 4 || strcmp(&output_filename[name_length - 5], ".jpeg")) {
+				printf("Output filename \'%s\' should have \'.jpeg\' extension.\n", output_filename);
 				return NULL;
 			}
-			jpeg_set_jpeg_filename(infos, &argv[i][10]);
+			jpeg_set_jpeg_filename(infos, output_filename);
 			outfile_flag = true;
 		}
-		else if (strncmp(argv[i], "--sample=", 9) == 0) {
+		/* --sample option */
+		else if (!strncmp(argv[i], "--sample=", 9)) {
 			if (sample_flag) {
-				printf("--sample option should only be called once.\n");
+				printf("\'--sample\' option should only be called once.\n");
 				return NULL;
 			}
 			sample_flag = true;
 		}
 		else {
-			printf("%s is not a valid option.\n", argv[i]);
-			print_wrong(argv);
+			printf("\'%s\' is not a valid option.\n", argv[i]);
+			printf("Try : \'%s --help\' for more information.\n", argv[0]);
 			return NULL;
 		}
 	}
-	jpeg_set_ppm_filename(infos, argv[argc - 1]);
+	char *input_filename = argv[argc - 1];
+	size_t name_length = strlen(input_filename);
+	if (name_length < 4 || (strcmp(&input_filename[name_length - 4], ".ppm") && strcmp(&input_filename[name_length - 4], ".pgm"))) {
+		printf("Input file \'%s\' does not have \'.ppm\' or \'.pgm\' extension.\n", input_filename);
+		return NULL;
+	}
+	FILE *image = fopen(input_filename, "r");
+	if (image == NULL) {
+		printf("Input file \'%s\' does not exist.\n", input_filename);
+		return NULL;
+	}
+	fclose(image);
+	jpeg_set_ppm_filename(infos, input_filename);
+	if (!outfile_flag) {
+		char output_filename[name_length + 2];
+		strcpy(output_filename, input_filename);
+		strcpy(&output_filename[name_length - 4], ".jpeg");
+		jpeg_set_jpeg_filename(infos, output_filename);
+	}
 	return infos;
 }
