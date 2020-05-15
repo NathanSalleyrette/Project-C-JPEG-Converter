014e088596e2b4f4fcba75116ee486224021dcb1
marionm <redmat527@PC-MATHIS-MARION.localdomain>
2020-05-15 14:58:58 +0200

    Ajout pour de vrai (oubli de git add).

---
+++ src/mcu.c
@@ -0,0 +1,62 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <ctype.h>
+#include <jpeg_writer.h>
+#include <mcu.h>
+
+struct array_mcu *get_mcu_from_jpeg(struct jpeg *jpeg)
+{
+	struct array_mcu *mcus = malloc(sizeof(struct array_mcu));
+	mcus->sf = malloc(6 * sizeof(uint8_t));
+	mcus->sf[0] = jpeg_get_sampling_factor(jpeg, Y, H);
+	mcus->sf[1] = jpeg_get_sampling_factor(jpeg, Y, V);
+	/* Before subsampling, sampling factors for Cb and Cr are set to the same value as for Y */
+	for (uint8_t i = 2; i < 5; i += 2) {
+		mcus->sf[i] = mcus->sf[0];
+		mcus->sf[i + 1] = mcus->sf[1];
+	}
+
+	mcus->ct = jpeg_get_nb_components(jpeg);
+	mcus->width = (jpeg_get_image_width(jpeg) + (mcus->sf[0] << 3) - 1) / (mcus->sf[0] << 3);
+	mcus->height = (jpeg_get_image_height(jpeg) + (mcus->sf[1] << 3) - 1) / (mcus->sf[1] << 3);
+
+	/* Creating the data array */
+	uint32_t mcu_pixel_width = mcus->sf[0] << 3;
+	uint32_t mcu_pixel_height = mcus->sf[1] << 3;
+	uint32_t pixel_per_mcu = mcu_pixel_width * mcu_pixel_height;
+	mcus->data = malloc(mcus->ct * sizeof(int16_t *));
+	for (uint8_t i = 0; i < mcus->ct; i++) {
+		mcus->data[i] = malloc(pixel_per_mcu * mcus->width * mcus->height * sizeof(int16_t));
+	}
+
+	FILE *image = fopen(jpeg_get_ppm_filename(jpeg), "r");
+	/* Reading passed the header */
+	for (uint8_t i = 0; i < 4; i++) {
+		while (!isspace(fgetc(image)));
+	}
+	/* Reading all pixels and writing them into mcu->data in the right order */
+	for (uint32_t y_mcu = 0; y_mcu < mcus->height; y_mcu++) {
+		for (uint32_t y_pixel = 0; y_pixel < mcu_pixel_height; y_pixel++) {
+			for (uint32_t x_mcu = 0; x_mcu < mcus->width; x_mcu++){		
+				for (uint32_t x_pixel = 0; x_pixel < mcu_pixel_width; x_pixel++) {
+					for (uint8_t component = 0; component < mcus->ct; component++) {
+						if (feof(image)) {
+							printf("Input file \'%s\' is not correctly encoded. (Less values encountered than expected)\n", jpeg_get_ppm_filename(jpeg));
+							return NULL;
+						}
+						mcus->data[component][pixel_per_mcu * (x_mcu + mcus->width * y_mcu) + x_pixel + mcu_pixel_width * y_pixel] = (uint16_t) fgetc(image);
+					}
+
+				}
+			}
+		}
+	}
+	fgetc(image);
+	if (!feof(image)) {
+		printf("Input file \'%s\' is not correctly encoded. (More values encountered than expected)\n", jpeg_get_ppm_filename(jpeg));
+		return NULL;
+	}
+	fclose(image);
+	return mcus;
+}
